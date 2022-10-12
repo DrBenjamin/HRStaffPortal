b39bda3d34fbc0caa3a3475577318bd67a168ca7
Benjamin Gross <benjamin.gross@giz.de>
2022-10-12 18:18:23 +0200

    added sub page landing (multi page support)

---
+++ pages/landing.py
@@ -0,0 +1,16 @@
+##### Kamazu Central Hospital (KCH) HR Staff Portal Prototype
+##### Open-Source, hostet on https://github.com/DrBenjamin/HRStaffPortal
+##### Please reach out to benjamin.gross@giz.de for any questions
+
+
+#### Loading neded Python libraries
+import streamlit as st
+import streamlit.components.v1 as stc
+import pandas as pd
+import numpy as np
+import math
+from datetime import datetime
+import mysql.connector
+import sys
+
+st.title('Landing page')
