f3547263af82990ae3c18dbaadb67f46746e5771
Benjamin Gross <benjamin.gross@giz.de>
2022-10-13 16:50:09 +0200

    added function to download employee image

--- 🏥_HR_Staff_Portal.py
+++ 🏥_HR_Staff_Portal.py
@@ -4,6 +4,7 @@
 ##### Please reach out to benjamin.gross@giz.de for any questions
 
 
+
 #### Loading neded Python libraries
 import streamlit as st
 import streamlit.components.v1 as stc
@@ -12,6 +13,7 @@ import numpy as np
 import mysql.connector
 import sys
 import webbrowser
+
 
 
 #### Streamlit initial setup
@@ -28,115 +30,126 @@ st.set_page_config(
 )
 
 
+
 #### Query parameters
-# Get param `EMPLOYE_NO`
+## Get param `EMPLOYE_NO`
 eno = st.experimental_get_query_params()
 
-# Get params for trainings / workshops
+## Get params for trainings / workshops
 # [code]
 
-#### Initialization of Session States
-# First Run State
-if ('success' not in st.session_state):
-  st.session_state['run'] = True
-# Database Transmition Success State
-if ('success' not in st.session_state):
-  st.session_state['success'] = False
-# Selected Employee Session State
-if ('index' not in st.session_state):
-  st.session_state['index'] = 0
-if ('menu' not in st.session_state):
-  st.session_state['menu'] = "About"
+  
 
-
-#### All Functions used
-### Password / User checking
+#### All Functions used in HRStaffPortal
+### Function: Password / User checking
 def check_password():
-    """Returns `True` if the user had a correct password."""
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if (
-            st.session_state["username"] in st.secrets["passwords"]
-            and st.session_state["password"]
-            == st.secrets["passwords"][st.session_state["username"]]
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
-        else:
-            st.session_state["password_correct"] = False
-    
-    ## Sidebar
-    # Sidebar Header Image
-    st.sidebar.image('images/MoH.png')
-
-    if "password_correct" not in st.session_state:
-        # First run, show inputs for username + password.
-        # Show Header Text
-        #st.sidebar.title('You are welcome to the KCH HR Staff Portal')
-        st.sidebar.subheader('Please enter username and password')
-        st.sidebar.text_input("Username", on_change=password_entered, key="username")
-        st.sidebar.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        show_title = True
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.sidebar.text_input("Username", on_change=password_entered, key="username")
-        st.sidebar.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.sidebar.error("User not known or password incorrect!")
-        show_title = True
-        return False
+  # Returns `True` if the user had a correct password."""
+  def password_entered():
+    # Checks whether a password entered by the user is correct."""
+    if (st.session_state["username"] in st.secrets["passwords"] and st.session_state["password"] == st.secrets["passwords"][st.session_state["username"]]):
+      st.session_state["password_correct"] = True
+      del st.session_state["password"]  # don't store username + password
+      del st.session_state["username"]
     else:
-        # Password correct.
-        st.sidebar.success('You are logged in.', icon = "✅")
-        st.sidebar.info('You can close this menu')
-        show_title = False
-        return True
-      
-### Function to open sub-pages
-def url_change(url):
-  print('URL changed: ', url)
-  #url = st.get_url() + url
-  #url = f"/{pathlib.Path(__file__).stem}/" + url
-  #url = "http://localhost:8501/" + url
-  webbrowser.open(url)
+      st.session_state["password_correct"] = False
+    
+  ## Sidebar
+  # Sidebar Header Image
+  st.sidebar.image('images/MoH.png')
 
-### SQL Connection
-# Initialize connection
+  if "password_correct" not in st.session_state:
+    # First run, show inputs for username + password
+    # Show Header Text
+    st.sidebar.subheader('Please enter username and password')
+    st.sidebar.text_input(label = "Username", on_change = password_entered, key = "username")
+    st.sidebar.text_input(label = "Password", type = "password", on_change = password_entered, key = "password")
+    return False
+  
+  elif not st.session_state["password_correct"]:
+    # Password not correct, show input + error
+    st.sidebar.text_input(label = "Username", on_change=password_entered, key = "username")
+    st.sidebar.text_input(label = "Password", type = "password", on_change = password_entered, key = "password")
+    if (st.session_state['logout']):
+      st.sidebar.success('Logout successful!', icon = "✅")
+    else:
+      st.sidebar.error(body = "User not known or password incorrect!", icon = "🚨")
+    return False
+  
+  else:
+    # Password correct
+    st.sidebar.success(body = 'You are logged in.', icon = "✅")
+    st.sidebar.info(body = 'You can close this menu')
+    st.sidebar.button(label = 'Logout', on_click = logout)
+    return True
+ 
+      
+### Logout Button
+def logout():
+  ## Set Logout to get Logout-message
+  st.session_state['logout'] = True
+  ## Logout
+  st.session_state["password_correct"] = False
+  
+
+### Function: SQL Connection
+## Initialize connection
 def init_connection():
   return mysql.connector.connect(**st.secrets["mysql"])
-# Perform query
+## Perform query
 def run_query(query):
   with conn.cursor() as cur:  
     cur.execute(query)
     return cur.fetchall()
 
-### Picture-uploader
+
+### Function: Picture-uploader
 def pictureUploader(image, index):
-  # Initialize connection
+  ## Initialize connection
   connection = mysql.connector.connect(**st.secrets["mysql"])
   cursor = connection.cursor()
-  # SQL statement
+  ## SQL statement
   sql_insert_blob_query = """ UPDATE IMAGEBASE SET IMAGE = %s WHERE ID = %s;"""
-  # Convert data into tuple format
+  ## Convert data into tuple format
   insert_blob_tuple = (image, index)
   result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
   connection.commit()
+ 
   
-### Convert digital data to binary format
+### Function: Convert digital data to binary format
 def loadFile(filename):
   with open(filename, 'rb') as file:
     binaryData = file.read()
   return binaryData
 
-### Write binary data on Hard Disk
+
+### Function: Write binary data on Hard Disk
 def writeFile(data, filename):
   with open(filename, mode = 'wb') as file:
     file.write(data)
+    
+    
+    
+#### Initialization of Session States
+## First Run State
+if ('success' not in st.session_state):
+  st.session_state['run'] = True
+  
+## Database Transmition Success State
+if ('success' not in st.session_state):
+  st.session_state['success'] = False
+  
+## Selected Employee Session State
+if ('index' not in st.session_state):
+  st.session_state['index'] = 0
+  
+## Logout
+if ('logout' not in st.session_state):
+  st.session_state['logout'] = False
+  
+## Current Image data
+if ('image' not in st.session_state):
+  st.session_state['image'] = loadFile('images/No_Image.png')
+
 
 
 #### Two Versions of the page -> Landing page vs. HRStaffPortal
@@ -151,9 +164,11 @@ if check_password():
       st.write('All the employee data is stored in a local MySQL databank on a Raspberry Pi.')
       st.write('The HR Portal is running on Streamlit, an Open Source Python framework for visualisation.')
 
+
     ## Use local databank idcard with Table ImageBase (EasyBadge polluted)
     # open Databak Connection
     conn = init_connection()
+
 
     ## Checkbox for option to see databank data
     query = "SELECT ID, LAYOUT, FORENAME, SURNAME, JOB_TITLE, EXPIRY_DATE, EMPLOYEE_NO, CARDS_PRINTED FROM `idcard`.`IMAGEBASE`;"
@@ -162,11 +177,13 @@ if check_password():
     for row in rows:
       df = pd.DataFrame([[row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]]], columns = ['ID', 'LAYOUT', 'FORENAME', 'SURNAME', 'JOB_TITLE', 'EXPIRY_DATE', 'EMPLOYEE_NO', 'CARDS_PRINTED'])
       databank = databank.append(df)
+ 
     
     ## Print databank in dataframe table
     with st.expander("See all Databank entries", expanded = False):
       databank = databank.set_index('ID')
       st.dataframe(databank, use_container_width = True)
+
       
     ## Get employee data for searching for building `ID` / `EMPLOYEE` pairs and filling the employee Selectbox
     query = "SELECT ID, FORENAME, SURNAME, EMPLOYEE_NO, JOB_TITLE FROM `idcard`.`IMAGEBASE`;"
@@ -184,15 +201,18 @@ if check_password():
       # Concenate Forename and Surname for Sidebar Selectbox
       names.append(str(row[1] + ' ' + row[2] + ' ' + row[3] + ' ' + row[4]))
 
+
     ## Employee Selectbox (on change sets first start session state)
     def onChange():
       st.session_state.run = True
     index = st.selectbox(label = "Which Employee do you want to select?", options = range(len(names)), format_func = lambda x: names[x], on_change = onChange, index = st.session_state.index)
+ 
     
     ## Checkboxes for editing and adding Training data
     if (index != 0):
       checkbox_val = st.checkbox(label = 'Edit Mode', value = False)
       checkbox_training = st.checkbox(label = 'Add Training', value = checkbox_val, disabled = not checkbox_val)
+
     
     ## Form for showing Employee input fields 
     with st.form("Employee", clear_on_submit = True):
@@ -218,6 +238,7 @@ if check_password():
         # If first entry in database start with `ID` `1`
         if (id == 0):
           id = 1
+
         
         ## Input for new employee data
         id = st.text_input(label = 'ID', value = id, disabled = True)
@@ -228,8 +249,7 @@ if check_password():
         exp = st.text_input(label = 'Expirity Date', value = '2023-12-31 00:00:00')
         eno = st.text_input(label = 'Employee Number', placeholder = 'Employee Number?')
         capri = st.text_input(label = 'Cards Printed', value = 0)
-        uploaded_file = st.file_uploader(label = "Upload a picture (256×360)", type = ['png', 'jpg'])
-        
+        uploaded_file = st.file_uploader(label = "Upload a picture (256×360)", type = 'png')
         if uploaded_file is not None:
           image = uploaded_file.getvalue()
           
@@ -240,7 +260,7 @@ if check_password():
         ## Submit Button `Create New Employee`
         submitted = st.form_submit_button("Create New Employee")
         if submitted:
-          # Writing to databank if data was entered
+          ## Writing to databank if data was entered
           if (layout is not None and forename and surname and job and exp and eno and capri):
             query = "INSERT INTO `idcard`.`IMAGEBASE`(ID, LAYOUT, FORENAME, SURNAME, JOB_TITLE, EXPIRY_DATE, EMPLOYEE_NO, CARDS_PRINTED) VALUES (%s, %s, '%s', '%s', '%s', '%s', %s, %s);" %(id, layout, forename, surname, job, exp, eno, capri)
             run_query(query)
@@ -248,7 +268,6 @@ if check_password():
             st.session_state.success = True
             
             # Upload picture to database
-            print("Uploading image at ID ", id)
             pictureUploader(image, id)
             
             # Set query parameter
@@ -261,14 +280,15 @@ if check_password():
             st.session_state.success = False
           
           st.experimental_rerun()
+ 
       
       ## If data is already existent, show filled form  
       else:
-        # Get information of selected Employee
+        ## Get information of selected Employee
         query = "SELECT ID, LAYOUT, FORENAME, SURNAME, JOB_TITLE, EXPIRY_DATE, EMPLOYEE_NO, CARDS_PRINTED, IMAGE FROM `idcard`.`IMAGEBASE` WHERE ID = %s;" %(index)
         employee = run_query(query)
         
-        # Input for updating employee data
+        ## Input for updating employee data
         updataMaster = False
         id = st.text_input(label = 'ID', value = employee[0][0], disabled = True)
         layout = st.text_input(label = 'Layout', value = employee[0][1], disabled = True)
@@ -290,30 +310,41 @@ if check_password():
         capri = st.text_input(label = 'Cards Printed', value = employee[0][7], disabled = not checkbox_val)
         if (employee[0][7] != capri):
           updateMaster = True
-        # Check if image is empty and show a placeholder
+          
+        ## Check if image is empty and show a placeholder
         if (len(employee[0][8]) < 10):
+          # Show placeholder
           st.image('images/portrait-placeholder.png')
+          # Set Image Session State to `No Image` placeholder
+          st.session_state['image'] = loadFile('images/No_Image.png')
+          
+        ## Show existing Image
         else:          
           st.image(employee[0][8])
-
-        uploaded_file = st.file_uploader(label = "Upload a picture (256×360)", type = ['png', 'jpg'], disabled = not checkbox_val)
+          # Save Image for downloading to Image Session State
+          st.session_state.image = employee[0][8]
+        
+        ## Image Uploader
+        uploaded_file = st.file_uploader(label = "Upload a picture (256×360)", type = 'png', disabled = not checkbox_val)
         if uploaded_file is not None:
           updateMaster = True
           image = uploaded_file.getvalue()
           # Upload picture to database
           pictureUploader(image, index)
-          
+        
+        ## No image data  
         else:
           image = ''
         
-        # Set query parameter
+        ## Set query parameter
         st.experimental_set_query_params(eno=eno)
           
+
+        ## Get information of selected Employee regarding Training
         with st.expander("Training Data", expanded = checkbox_training):
-          ## Get information of selected Employee regarding Training
           st.title('Employee Training Data')
           
-          # Check for last ID number in TrainingData (to add data after)
+          ## Check for last ID number in TrainingData (to add data after)
           idT = 0
           query = "SELECT ID from `idcard`.`TRAININGDATA`;"
           rows = run_query(query)
@@ -324,21 +355,21 @@ if check_password():
           if (idT == 0):
             idT = 1
           
-          # Get Training Data
+          ## Get Training Data
           query = "SELECT tr.TRAINING, tr.INSTITUTE, tr.DATE, tr.DAYS, tr.ID FROM `idcard`.`IMAGEBASE` AS ima LEFT JOIN `idcard`.`TRAININGDATA` AS tr ON ima.EMPLOYEE_NO = tr.EMPLOYEE_NO WHERE ima.ID = %s;" %(index)
           trainingData = run_query(query)
           
-          # Variables for Text Input
+          ## Variables for Text Input
           training = []
           institute = []
           date = []
           days = []
           
-          # Boolean for flow control
+          ## Boolean for flow control
           insert = False # Will be set to `True` if a new entry is entered
           update = False # Will be set to `True` if existing data is altered
           
-          # Check if Training Data is already there for an Employee and show it
+          ## Check if Training Data is already there for an Employee and show it
           if (trainingData[0][0] != None):
             update = False
             for i in range(len(trainingData)):
@@ -359,21 +390,22 @@ if check_password():
               if (trainingData[i][3] != x):
                 update = True
               days.append(x)
+  
               
           ## Show new entry input fields if checkbox 'Add Training' is checked
           if not checkbox_training:
             if (trainingData[0][0] == None):
-              st.info('No Training Data available', icon="ℹ️")
+              st.info(body = 'No Training Data available', icon = "ℹ️")
               
           else:
-            # Calculating number of training
+            ## Calculating number of training
             if (trainingData[0][0] == None):
               counter = 'Training #1'
               
             else:
               counter = 'Training #' + str(len(trainingData) + 1)
               
-            # Inputs for new Training
+            ## Inputs for new Training
             x = st.text_input(label = counter, placeholder = 'Training?', disabled = not checkbox_val)
             if x.strip():
               training.append(x)
@@ -390,13 +422,14 @@ if check_password():
             if x.strip():
               days.append(x)
               insert = True
+   
             
         ## Warning or Success messages after reloading
         if (st.session_state.run != True and st.session_state.success == True):
-          st.success('Data submitted to Databank.', icon="✅")
+          st.success(body = 'Data submitted to Databank.', icon = "✅")
         else:
           if (st.session_state.run != True):
-            st.warning('Not sumitted, as no new Data was entered!', icon="⚠️")
+            st.warning(body = 'Not sumitted, as no new Data was entered!', icon = "⚠️")
         
         ## Submit Button for Changes
         submitted = st.form_submit_button("Save Changes")
@@ -414,8 +447,9 @@ if check_password():
             
           else:
             st.session_state.success = False
+     
           
-          # Writing to databank idcard Table TRAININGDATA - new first Entry
+          ## Writing to databank idcard Table TRAININGDATA - new first Entry
           if (insert == True and update == False):
             st.write(training[0], institute[0], date[0], days[0])
             if (training[0].strip() and institute[0].strip() and date[0].strip() and days[0].strip()):
@@ -427,7 +461,8 @@ if check_password():
             else:
               st.session_state.success = False
               
-          # Writing to databank idcard Table TRAININGDATA - new Entries (not first)
+              
+          ## Writing to databank idcard Table TRAININGDATA - new Entries (not first)
           if (insert == True and update == True):
             st.write(training[0], institute[0], date[0], days[0])
             st.write(len(trainingData))
@@ -439,7 +474,8 @@ if check_password():
             else:
               st.session_state.success = False
               
-          # Writing to databank idcard Table TRAININGDATA - Updates to all existing entries
+              
+          ## Writing to databank idcard Table TRAININGDATA - Updates to all existing entries
           if (update == True):
             for i in range(len(trainingData)):
               st.write(training[i], institute[i], date[i], days[i])
@@ -448,20 +484,30 @@ if check_password():
               run_query(query)
               conn.commit()
             st.session_state.success = True
+
           
-          # Set Session State to 2nd run and reloading to get actual data
+          ## Set Session State to 2nd run and reloading to get actual data
           st.session_state.run = False
           st.experimental_rerun()
+
+
+          
+    ### Out of the Form
+    ## Image Download Button
+    st.download_button('Download Image', data = st.session_state.image, mime="image/png")
+
+ 
           
           
 #### Not Logged in state (Landing page)
 else :
   ### Header of Landing Page
-  ## Title
+  ## Title and information
   st.title('Kamuzu Central Hospital (KCH)')
   st.header('Welcome to the HR Staff Portal')
   st.subheader('User Login')
   st.write('Please login (sidebar on the left) to access the KCH HR Staff Portal.')
+  
   
   ## Sub-pages menu
   ## Menu to open sub-pages  
