22736d23a5c06686fd4ecb1a5bfed5d0c61d3320
Benjamin Gross <benjamin.gross@giz.de>
2022-10-13 07:51:59 +0200

    renamed `Landing.py` to `LandingPage.py`

--- pages/LandingPage.py
+++ pages/LandingPage.py
@@ -1,3 +1,4 @@
+# LandingPage.py
 ##### Kamazu Central Hospital (KCH) HR Staff Portal Prototype
 ##### Open-Source, hostet on https://github.com/DrBenjamin/HRStaffPortal
 ##### Please reach out to benjamin.gross@giz.de for any questions
