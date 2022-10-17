3f72dfab7e23b90673182373e9d0e5de52d19c4e
Benjamin Gross <benjamin.gross@giz.de>
2022-10-17 19:03:11 +0200

    testing

--- pages/2 - 🛠_Workshops.py
+++ pages/2 - 🛠_Workshops.py
@@ -34,17 +34,7 @@ st.title('Workshop Page')
 
 ## Shillelagh
 connect(':memory:', adapters = 'csvfile')
-<<<<<<< HEAD
-<<<<<<< HEAD
 cursor = connect.cursor()
-=======
-cursor = connection.cursor()
-connect(':memory:', adapters = 'csvfile')
->>>>>>> e7bcaa856d3c83aabc9fb89d9d9bd9c60a277466
-=======
-cursor = connection.cursor()
-connect(':memory:', adapters = 'csvfile')
->>>>>>> e7bcaa856d3c83aabc9fb89d9d9bd9c60a277466
 query = "SELECT * FROM 'test.csv';"
 csvData = cursor.execute(query)
 st.dataframe(csvData, use_container_width = True)
