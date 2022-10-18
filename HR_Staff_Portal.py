d5128fdce0b23c430b02e0005dfe1ab122cdad21
Benjamin Gross <benjamin.gross@giz.de>
2022-10-18 12:46:57 +0200

    test

--- HR_Staff_Portal.py
+++ HR_Staff_Portal.py
@@ -360,7 +360,7 @@ if check_password():
           trainingData = []
           for row in rows:
             st.write('Row: ', row[0], ' ' , row[1], ' ', row[2], ' ', row[3], ' ', row[4])
-            trainingData = trainingData.append([[row[0], row[1], row[2], row[3], row[4]]])
+            trainingData.append([[row[0], row[1], row[2], row[3], row[4]]])
             st.write(trainingData)
 
             
