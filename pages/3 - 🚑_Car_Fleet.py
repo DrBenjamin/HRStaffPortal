1e0fd01ad6694f937843b4eb8defd5c083650c36
DrBenjamin <ben@benbox.org>
2022-11-11 11:54:03 +0200

    added car fleet file

---
+++ pages/3 - 🚑_Car_Fleet.py
@@ -0,0 +1,9 @@
+##### `🚑_Car_Fleet.py`
+##### Kamazu Central Hospital (KCH) HR Staff Portal Prototype
+##### Open-Source, hostet on https://github.com/DrBenjamin/HRStaffPortal
+##### Please reach out to benjamin.gross@giz.de for any questions
+
+import streamtlit as st
+
+
+st.title("KCH Car Fleet")
