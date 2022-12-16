abe9de480a351c0b3bc5affc75d9f9655fdd7b40
Benjamin Gross <benjamin.gross@giz.de>
2022-12-16 14:08:11 +0200

    added `BenBox.png` image

--- pages/2 - 🛠_Workshops.py
+++ pages/2 - 🛠_Workshops.py
@@ -61,21 +61,26 @@ lang = st.sidebar.selectbox('In which language should Ben answer?', ('BG', 'CS',
 st.title('Workshop Page')
 st.subheader('Ben chatbot')
 
-
-## Implementation of OpenAI
-question = st.text_input(label = 'What question to Ben do you have?', placeholder = 'What is a Workshop?')
-question = 'Write an extremely long, detailed answer to \"' + question + '?"'
-if (question != 'Write an extremely long, detailed answer to "?"'):
-  response = openai.Completion.create(engine = "text-davinci-002", prompt = question, temperature = 0.7, max_tokens = 709, top_p = 1, frequency_penalty = 0, presence_penalty = 0)
-  
-  # Translation
-  if (lang != 'EN-GB'):
-    answer = trans(input = response, target_lang = lang)
-  else:
-    answer = response
-  
-  # Output
-  st.write(answer)
+# Columns
+col1, col2 = st.columns(2)
+with col1:
+  ## Implementation of OpenAI
+  question = st.text_input(label = 'What question to Ben do you have?', placeholder = 'What is a Workshop?')
+  question = 'You are Ben and you lives in a box. You give funny and and a bit sarcastic answers. You have a brother, which is also an AI but totally evil and who is not on the spaceship. You live in the future and are part of a crew of a spaceship which name is Pulp. The Pulp is a lonely spaceship and is on a mission to find ancient relics and artefacts. Your human colleagues are Emmi, Sertan. Emmi is a mechanic and Sertan good in computer technology. There are also the robots SAM and SEB with us on the Pulp. Write an long and detailed answer to \"' + question + '?"'
+  if (question != 'Write an extremely long, detailed answer to "?"'):
+    response = openai.Completion.create(model = "text-davinci-003", prompt = question, temperature = 0.5, max_tokens = 256, top_p = 0.3, frequency_penalty = 0.5, presence_penalty = 0)
+    
+    # Translation
+    if (lang != 'EN-GB'):
+      answer = trans(input = response["choices"][0]["text"], target_lang = lang)
+    else:
+      answer = response["choices"][0]["text"]
+    
+    # Output
+    st.write(answer)
+    
+with col2:
+  st.image("images/BenBox.png")
 
 
 
