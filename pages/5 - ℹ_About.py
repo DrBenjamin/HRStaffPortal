370ae1ae6e5ca2cd49819afaca3cf21e390e860b
Benjamin Gross <benjamin.gross@giz.de>
2022-12-17 09:53:29 +0200

    code beautifying

--- pages/5 - ℹ_About.py
+++ pages/5 - ℹ_About.py
@@ -2,9 +2,6 @@
 ##### Kamazu Central Hospital (KCH) HR Staff Portal Prototype
 ##### Open-Source, hostet on https://github.com/DrBenjamin/HRStaffPortal
 ##### Please reach out to benjamin.gross@giz.de for any questions
-
-
-
 #### Loading neded Python libraries
 import streamlit as st
 import streamlit.components.v1 as stc
