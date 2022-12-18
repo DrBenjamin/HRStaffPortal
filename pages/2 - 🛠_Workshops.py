63b75de0c50151e9faf53e31dc8310d7404701f0
Benjamin Gross <benjamin.gross@giz.de>
2022-12-18 08:36:42 +0200

    added a keyword extracting openai

--- pages/2 - 🛠_Workshops.py
+++ pages/2 - 🛠_Workshops.py
@@ -5,7 +5,7 @@
 #### Loading neded Python libraries
 import streamlit as st
 import streamlit.components.v1 as stc
-import os
+import openai
 import sys
 sys.path.insert(1, "pages/functions/")
 
@@ -29,8 +29,34 @@ st.set_page_config(
 
 
 #### Functions
-
-
+### Function geo_check = Geo-Location checking
+def geo_check(address_part, fallback, language = 'en'):
+  ## Geocoder gets coordinates related to the current IP address
+  g = geocoder.ip('me')
+  
+  
+  ## Geopy
+  # Calling the Nominatim tool
+  loc = Nominatim(user_agent = "GetLoc", timeout = 3)
+  geolocator = Nominatim(user_agent = "geoapiExercises")
+  
+  # Get City name
+  try:
+    geolocator.reverse(g.latlng, language = lang)
+    location = geolocator.geocode(g.latlng, addressdetails = True)
+    loc = location.raw
+    output = loc['address'][address_part]
+    print("Detected location:", output)
+    
+  # Set `output` to `fallback` if not reachable
+  except:
+    print("An exception occurred in `Geocoder`")
+    output = fallback
+  
+  # Return output 
+  return output
+  
+  
 
 
 #### Sidebar
@@ -43,5 +69,42 @@ st.sidebar.image('images/MoH.png')
 
 #### Main Program
 ### Header
-## Title
-st.title('Workshop Page')
+## Header
+st.title('Workshop page')
+st.subheader('Ben chatbot')
+
+
+
+### Chat Bot Ben
+## Columns
+col1, col2 = st.columns(2)
+
+
+## Column 1
+with col1:
+  ## Implementation of OpenAI
+  text_input = st.text_input(label = 'What text should be summarised to one keyword?', placeholder = 'Salary, is what an employee get paid at the end of the month')
+  
+  # Concacenate question
+  text_prompt = 'Extract one keyword of the beginning of the text which is a noun without the accompanying article and pronoun: \"' + text_input + '"'
+  
+  
+  ## Get response from openai
+  # Set API key
+  openai.api_key = st.secrets["openai"]["key"]
+  
+  try:
+    response = openai.Completion.create(model = "text-curie-001", prompt = text_prompt, temperature = 0.0, max_tokens = 64, top_p = 1.0, frequency_penalty = 0.0, presence_penalty = 0.0)
+
+    summarization = response["choices"][0]["text"]
+  
+    # Output
+    st.write(summarization)
+    
+  except:
+    print("An exception occurred in `OpenAI`")
+    
+  
+## Column 1
+with col2:
+  st.image("images/Ben.png")
