18b80dded4741136187a4e1b12f9d062d46b020d
Benjamin Gross <benjamin.gross@giz.de>
2022-12-22 12:17:46 +0200

    adding function to add approved answer to table `FAQ`

--- pages/2 - 🛠_Workshops.py
+++ pages/2 - 🛠_Workshops.py
@@ -30,6 +30,21 @@ st.set_page_config(
 
 
 
+#### Initialization of session states
+## Answer State
+if ('success' not in st.session_state):
+  st.session_state['answer'] = ''
+ 
+  
+## Write to `FAQ` table
+if ('question_id' not in st.session_state):
+  st.session_state['question_id'] = ''
+if ('handbook_id' not in st.session_state):
+  st.session_state['handbook_id'] = ''
+  
+  
+
+  
 #### Functions
 ### Function: run_query = Initial SQL Connection
 def init_connection():
@@ -70,8 +85,24 @@ def lastID(url):
   
   # Return ID    
   return id
+
+
+
+### Function: generateID = Generates an 5-digits ID
+def generateID(id):
+  if (id < 10):
+    generated_id = '0000' + str(id)
+  elif (id < 100):
+    generated_id = '000' + str(id)
+  elif (id < 1000):
+    generated_id = '00' + str(id)
+  elif (id < 10000):
+    generated_id = '0' + str(id)
+    
+  # Return the 5-digit ID
+  return(generated_id)
   
-  
+
 
 
 #### Sidebar
@@ -91,8 +122,12 @@ st.subheader('Ben chatbot')
 
 ## Global variables
 answer = ''
-keyword1 = ''
-keywords = []
+question_id = '' #st.session_state['question_id']
+handbook_id = '' #st.session_state['handbook_id']
+#summary = ''
+#keyword1 = ''
+#keywords = []
+#databank_handbook = pd.DataFrame(columns = ['ID_Index', 'ID', 'HANDBOOK_ID', 'CATEGORY_ID', 'CATEGORY_SUB_ID', 'HANDBOOK_KEYWORD1', 'HANDBOOK_KEYWORD2', 'HANDBOOK_KEYWORD3', 'HANDBOOK_KEYWORD4', 'HANDBOOK_KEYWORD5', 'HANDBOOK_TEXT', 'HANDBOOK_HITS'])
 
 
 
@@ -128,12 +163,12 @@ with st.form('Input', clear_on_submit = True):
     # Sub-Category menu
     sub_category = st.selectbox(label = 'Please choose the sub-category of the question', options = range(len(sub_categories)), format_func = lambda x: sub_categories[x])
     
+    # User question template
+    st.write('Where do I get the login username and password for the HR Staff Portal from?')
+    
     # User question
     user_question = st.text_input(label = 'What text should be summarised to one keyword?', placeholder = 'Salary, is what an employee get paid at the end of the month')
     
-    # Concacenate questions
-    summary_question = 'A User asks in the category \"' + categories[category] + '\" and the sub-category"' + sub_categories[sub_category] + '" about this question: \"' + user_question + '" Please summarise it to a statement in no more than three words.'
-    keyword_question = 'Extract one keyword of the beginning of the text which is a noun without the accompanying article and pronoun: \"' + user_question + '"'
     
     
   ## Submit button
@@ -146,17 +181,20 @@ with st.form('Input', clear_on_submit = True):
     try:
       ## Doing the requests to OpenAI for summarizing / keyword extracting the question
       # Summary
+      # Concacenate questions
+      summary_question = 'A User asks in the category \"' + categories[category] + '\" and the sub-category"' + sub_categories[sub_category] + '" about this question: \"' + user_question + '" Please summarise it to a statement in no more than three words.'
       response_summary = openai.Completion.create(model = "text-davinci-003", prompt = summary_question, temperature = 0.3, max_tokens = 64, top_p = 1.0, frequency_penalty = 0.0, presence_penalty = 0.0)
       summary = response_summary['choices'][0]['text'].lstrip()
       summary = summary.replace('.', '')
       
       # Find the keyword1
+      keyword_question = 'Extract one one-word keyword of the beginning of the text which is a noun without the accompanying article and pronoun: \"' + user_question + '"'
       response_keyword = openai.Completion.create(model = "text-curie-001", prompt = keyword_question, temperature = 0.0, max_tokens = 64, top_p = 1.0, frequency_penalty = 0.0, presence_penalty = 0.0)
       keyword1 = response_keyword['choices'][0]['text'].lstrip()
       keyword1 = keyword1.capitalize()
       
       # Find the 4 other keywords 
-      keywords_question = 'Extract four one-word keywords which are not the same as \"' + keyword1 + '\"' + ' of this text: \"' + user_question + '"'
+      keywords_question = 'Extract four one-word keywords which are not the same as \"' + keyword1 + '\"' + ' of this text: \"' + user_question + '" Please indicate your answer separated by commas.'
       response_keywords = openai.Completion.create(model = "text-davinci-003", prompt = keywords_question, temperature = 0.0, max_tokens = 64, top_p = 1.0, frequency_penalty = 0.0, presence_penalty = 0.0)
       keywords = response_keywords['choices'][0]['text']
       
@@ -188,79 +226,139 @@ with st.form('Input', clear_on_submit = True):
       id = lastID(url = "benbox.QUESTIONS")
         
       # Pollute `QUESTION_ID`
-      if (id < 10):
-        question_id = '0000' + str(id)
-      elif (id < 100):
-        question_id = '000' + str(id)
-      elif (id < 1000):
-        question_id = '00' + str(id)
-      elif (id < 10000):
-        question_id = '0' + str(id)
+      question_id = generateID(id)
+      st.session_state['question_id'] = question_id
         
-      # Pollute `QUESTTION_TEXT_LANGUAGE
+      # Pollute `QUESTION_TEXT_LANGUAGE
       language = 'en'
         
-      # Write to table
+      # Write question to table `QUESTIONS`
       query = "INSERT INTO `benbox`.`QUESTIONS`(ID, QUESTION_ID, QUESTION_CATEGORY, QUESTION_CATEGORY_SUB, QUESTION_KEYWORD1, QUESTION_KEYWORD2, QUESTION_KEYWORD3, QUESTION_KEYWORD4, QUESTION_KEYWORD5, QUESTION_SUMMARY, QUESTION_TEXT, QUESTION_TEXT_LANGUAGE) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" %(id, question_id, categories_id[category], sub_categories_id[sub_category], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], summary, user_question, language)
       run_query(query)
       conn.commit()
       
       
-      ## Get handbook data to answer
-      query = "SELECT ID, ID, HANDBOOK_ID, HANDBOOK_KEYWORD1, HANDBOOK_KEYWORD2, HANDBOOK_KEYWORD3, HANDBOOK_KEYWORD4, HANDBOOK_KEYWORD5, HANDBOOK_TEXT, HANDBOOK_HITS FROM benbox.HANDBOOK_USER WHERE HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s';" % (keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3])
+      ## Get handbook data to answer from table `HANDBOOK_USER`
+      query = "SELECT ID, ID, HANDBOOK_ID, CATEGORY_ID, CATEGORY_SUB_ID, HANDBOOK_KEYWORD1, HANDBOOK_KEYWORD2, HANDBOOK_KEYWORD3, HANDBOOK_KEYWORD4, HANDBOOK_KEYWORD5, HANDBOOK_SUMMARY, HANDBOOK_TEXT, HANDBOOK_HITS FROM benbox.HANDBOOK_USER WHERE HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD1 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD2 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD3 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD4 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s' OR HANDBOOK_KEYWORD5 LIKE '%s';" % (keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3], keyword1, keywords[0], keywords[1], keywords[2], keywords[3])
       rows = run_query(query)
-      databank_handbook = pd.DataFrame(columns = ['ID_Index', 'ID', 'HANDBOOK_ID', 'HANDBOOK_KEYWORD1', 'HANDBOOK_KEYWORD2', 'HANDBOOK_KEYWORD3', 'HANDBOOK_KEYWORD4', 'HANDBOOK_KEYWORD5', 'HANDBOOK_TEXT', 'HANDBOOK_HITS'])
+      databank_handbook = pd.DataFrame(columns = ['ID_Index', 'ID', 'HANDBOOK_ID', 'CATEGORY_ID', 'CATEGORY_SUB_ID', 'HANDBOOK_KEYWORD1', 'HANDBOOK_KEYWORD2', 'HANDBOOK_KEYWORD3', 'HANDBOOK_KEYWORD4', 'HANDBOOK_KEYWORD5', 'HANDBOOK_SUMMARY', 'HANDBOOK_TEXT', 'HANDBOOK_HITS'])
       for row in rows:
-        df = pd.DataFrame([[row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]]], columns = ['ID_Index', 'ID', 'HANDBOOK_ID', 'HANDBOOK_KEYWORD1', 'HANDBOOK_KEYWORD2', 'HANDBOOK_KEYWORD3', 'HANDBOOK_KEYWORD4', 'HANDBOOK_KEYWORD5', 'HANDBOOK_TEXT', 'HANDBOOK_HITS'])
+        df = pd.DataFrame([[row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12]]], columns = ['ID_Index', 'ID', 'HANDBOOK_ID', 'CATEGORY_ID', 'CATEGORY_SUB_ID', 'HANDBOOK_KEYWORD1', 'HANDBOOK_KEYWORD2', 'HANDBOOK_KEYWORD3', 'HANDBOOK_KEYWORD4', 'HANDBOOK_KEYWORD5', 'HANDBOOK_SUMMARY', 'HANDBOOK_TEXT', 'HANDBOOK_HITS'])
         databank_handbook = pd.concat([databank_handbook, df])
       databank_handbook = databank_handbook.set_index('ID_Index')
-      
-      
-      ## Checking if he found handbook data
+
+    
+      ## Checking if handbook data was found
       if (len(databank_handbook) > 0):     
         # Adding keyword1 at first position
         keywords.insert(0, keyword1)
+          
+              
+        ## Loop through all matches
         for x in range(len(databank_handbook)):
           handbook = databank_handbook.iloc[x]
-          counter = 0 + int(databank_handbook._get_value(handbook[0], 'HANDBOOK_HITS'))
+                
+          # Add importance through `HANDBOOK_HITS` value
+          counter = 0 + handbook[11]
+            
+                
+          ## Check for matching category and sub-category
+          if (handbook[2] == categories_id[category]):
+            counter += 3
+            if (handbook[3] == sub_categories_id[sub_category]):
+              counter += 5
+            
+              
+          ## Counting for matching words in summary (score 4 per hit)
+          string1_words = set(handbook[9].split())
+          string2_words = set(summary.split())
+          
+          # Remove unwanted characters
+          unwanted_characters = ".,!?"
+          string1_words = {word.strip(unwanted_characters) for word in string1_words}
+          string2_words = {word.strip(unwanted_characters) for word in string2_words}
+          
+          # Check for matching words
+          matches = string1_words & string2_words
+          
+          # Adding matching words to `counter`
+          counter += 4 * len(matches)
+          
+          
+          ## Counting for matching words in text (score 1 per hit)
+          string1_words = set(handbook[10].split())
+          string2_words = set(user_question.split())
+          
+          # Remove unwanted characters
+          unwanted_characters = ".,!?"
+          string1_words = {word.strip(unwanted_characters) for word in string1_words}
+          string2_words = {word.strip(unwanted_characters) for word in string2_words}
+          
+          # Check for matching words
+          matches = string1_words & string2_words
+          
+          # Adding matching words to `counter`
+          counter += len(matches)
+            
+              
+          ## Do the keyword matching through all keywords
           for i in range(5):
-            if handbook[2].capitalize() == keywords[i]:
-              counter += 1
-            if handbook[3].capitalize() == keywords[i]:
-              counter += 1
+            # First keyword gets highest score
             if handbook[4].capitalize() == keywords[i]:
-              counter += 1
+              counter += 5
+                  
+            # Second and third get medium score
             if handbook[5].capitalize() == keywords[i]:
-              counter += 1
+              counter += 4
             if handbook[6].capitalize() == keywords[i]:
-              counter += 1
+              counter += 4
+                    
+            # Fourth and fith geth lowest score
+            if handbook[7].capitalize() == keywords[i]:
+              counter += 3
+            if handbook[8].capitalize() == keywords[i]:
+              counter += 3
             databank_handbook['HANDBOOK_HITS'][handbook[0]] = counter
-              
-              
+                  
+        st.write(databank_handbook)
+                    
+                    
         ## Checking for highest Score ('HANDBOOK_HITS')
         for x in range(len(databank_handbook)):
           if (x == 0):
             handbook = databank_handbook._get_value(databank_handbook.iloc[x][0], 'HANDBOOK_TEXT')
+            st.write('Running Nr. 1: ', handbook)
+            handbook_id = databank_handbook._get_value(databank_handbook.iloc[x][0], 'HANDBOOK_ID')
+            st.session_state['handbook_id'] = handbook_id
+            st.write('Running Nr. 1, 1: ', handbook_id)
           else:
             for i in range(x):
-              if (databank_handbook.iloc[x][8] <= databank_handbook.iloc[i][8]):
+              if (databank_handbook.iloc[x][11] <= databank_handbook.iloc[i][11]):
                 break
               else:
                 if (i == x - 1):
                   handbook = databank_handbook._get_value(databank_handbook.iloc[x][0], 'HANDBOOK_TEXT')
-                  
+                  st.write('Running Nr. x: ', handbook)
+                  handbook_id = databank_handbook._get_value(databank_handbook.iloc[x][0], 'HANDBOOK_ID')
+                  st.write('Handbook ID: ', hanbdbook_id)
+                  st.session_state['handbook_id'] = handbook_id
+      
                   
         ## Doing the request to OpenAI for answering the question
         # Answer
         answer_question = 'The user handbook contains following information: \"' + handbook + '\". Please answer following user question: \"' + user_question + '\"'
         response_answer = openai.Completion.create(model = "text-davinci-003", prompt = answer_question, temperature = 0.5, max_tokens = 128, top_p = 1.0, frequency_penalty = 0.0, presence_penalty = 0.0)
         answer = response_answer['choices'][0]['text'].lstrip()
+        st.session_state['answer'] = answer
+        
       else:
         answer = 'Unfortunately we have not found the answer to your question.' 
-      
+        st.session_state['answer'] = answer
       
     except:
       print('An exception occurred in `OpenAI`')
+      answer = 'Unfortunately we have not found the answer to your question.'
+      st.session_state['answer'] = answer
       
     
   ## Column 1
@@ -270,4 +368,52 @@ with st.form('Input', clear_on_submit = True):
   
   
 ### Outside the form
-st.write(answer)
+## Give the answer
+# Pollute `FAQ_ANSWER`
+answer = st.session_state['answer']
+st.write('Length: ', len(answer))
+if (len(answer) > 0):
+  st.write(':blue[Ben:] ', answer)
+
+
+  ## Writing to database if it was useful
+  if (answer != 'Unfortunately we have not found the answer to your question.'):
+    # Ask if it was useful
+    st.write('Was this answer useful?')
+  
+  
+    ## Write to table `FAQ` if it was useful
+    if st.button(label = 'Yes'):
+      # Get latest ID from database
+      id = lastID(url = "benbox.FAQ")
+      
+      # Pollute `QUESTION_ID`
+      question_id = st.session_state['question_id']
+      
+      # Pollute `HANDBOOK_ID`
+      handbook_id = st.session_state['handbook_id']
+      
+      # Pollute `FAQ_ANSWER`
+      answer = st.session_state['answer']
+      
+      # Pollute `FAQ_ANSWER_LANGUAGE
+      faq_id = generateID(id)
+      
+      # Pollute `FAQ_ANSWER_LANGUAGE
+      language = 'en'
+      
+      # Summarising the answer
+      answer_summary_question = 'Please summarise this text in no more than seven words: \"' + answer + '\"'
+      response_answer_summary = openai.Completion.create(model = "text-curie-001", prompt = answer_summary_question, temperature = 0.3, max_tokens = 128, top_p = 1.0, frequency_penalty = 0.0, presence_penalty = 0.0)
+      answer_summary = response_answer_summary['choices'][0]['text'].lstrip()
+        
+      # Write question to table `FAQ`
+      query = "INSERT INTO `benbox`.`FAQ`(ID, QUESTION_ID, HANDBOOK_ID, FAQ_ID, FAQ_ANSWER, FAQ_ANSWER_SUMMARY, FAQ_ANSWER_LANGUAGE, FAQ_HITS) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', %s);" %(id, question_id, handbook_id, faq_id, answer, answer_summary, language, 0)
+      st.write(query)
+      run_query(query)
+      conn.commit()
+      
+      # Response to user
+      st.write('Thanks for your feedback, it was recorded.')
+    else:
+      st.write('Your feedback will be recorded.')
