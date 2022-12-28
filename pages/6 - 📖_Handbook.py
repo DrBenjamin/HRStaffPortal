623b1e1f656f96a0e7f1d0f7227aa5cea2eb36c3
Benjamin Gross <benjamin.gross@giz.de>
2022-12-28 17:23:18 +0100

    added handbook data entry page

--- pages/6 - 📖_Handbook.py
+++ pages/6 - 📖_Handbook.py
@@ -55,14 +55,14 @@ def run_query(query):
       
       
       
-### Function: pictureUploader = uploads employee images
+### Function: pictureUploader = uploads handbook images
 def pictureUploader(image, index):
   # Initialize connection
-  connection = mysql.connector.connect(**st.secrets["mysql"])
+  connection = mysql.connector.connect(**st.secrets["mysql_benbox"])
   cursor = connection.cursor()
   
   # SQL statement
-  sql_insert_blob_query = """ UPDATE IMAGEBASE SET IMAGE = %s WHERE ID = %s;"""
+  sql_insert_blob_query = """ UPDATE `HANDBOOK_USER` SET HANDBOOK_IMAGE = %s WHERE ID = %s;"""
   
   # Convert data into tuple format
   insert_blob_tuple = (image, index)
@@ -153,60 +153,52 @@ category = st.selectbox(label = 'Please choose the category of the handbook entr
 sub_category = st.selectbox(label = 'Please choose the sub-category of the handbook entry', options = range(len(sub_categories)), format_func = lambda x: sub_categories[x])
      
       
-## Columns
-col1, col2 = st.columns(2)
-
-
-## Column 1
-with col1:
-  with st.form('Input', clear_on_submit = False):
-    ## Handbook data entry
-    # Open databank connection
-    conn = init_connection()
+## Form
+with st.form('Input', clear_on_submit = True):
+  ## Handbook data entry
+  # Open databank connection
+  conn = init_connection()
     
     
-    ## Input for new `HANDBOOK_USER` data
-    # Get latest ID from table
-    id = lastID(url = '`benbox`.`HANDBOOK_USER`')
-    handbook_id = generateID(id)
-    st.text_input(label = 'ID', value = id, disabled = True)
-    st.text_input(label = 'Handbook ID', value = handbook_id, disabled = True)
-    st.text_input(label = 'Category ID', value = categories_id[category], disabled = True)
-    st.text_input(label = 'Sub-Category ID', value = sub_categories_id[sub_category], disabled = True)
-    handbook_chapter = st.text_input(label = 'Chapter', placeholder = 1)
-    handbook_paragraph = st.text_input(label = 'Paragraph', placeholder = 1)
-    handbook_keyword1 = st.text_input(label = 'Keyword 1')
-    handbook_keyword2 = st.text_input(label = 'Keyword 2')
-    handbook_keyword3 = st.text_input(label = 'Keyword 3')
-    handbook_keyword4 = st.text_input(label = 'Keyword 4')
-    handbook_keyword5 = st.text_input(label = 'Keyword 5')
-    handbook_summary = st.text_input(label = 'Summary')
-    handbook_text = st.text_input(label = 'Text')
-    handbook_text_language = st.text_input(label = 'Language', value = 'en')
-    handbook_hits = st.text_input(label = 'Hits', value = 0)
+  ## Input for new `HANDBOOK_USER` data
+  # Get latest ID from table
+  id = lastID(url = '`benbox`.`HANDBOOK_USER`')
+  handbook_id = generateID(id)
+  st.text_input(label = 'ID', value = id, disabled = True)
+  st.text_input(label = 'Handbook ID', value = handbook_id, disabled = True)
+  st.text_input(label = 'Category ID', value = categories_id[category], disabled = True)
+  st.text_input(label = 'Sub-Category ID', value = sub_categories_id[sub_category], disabled = True)
+  handbook_chapter = st.text_input(label = 'Chapter', value = 1)
+  handbook_paragraph = st.text_input(label = 'Paragraph', value = 1)
+  handbook_keyword1 = st.text_input(label = 'Keyword 1')
+  handbook_keyword2 = st.text_input(label = 'Keyword 2')
+  handbook_keyword3 = st.text_input(label = 'Keyword 3')
+  handbook_keyword4 = st.text_input(label = 'Keyword 4')
+  handbook_keyword5 = st.text_input(label = 'Keyword 5')
+  handbook_summary = st.text_input(label = 'Summary')
+  handbook_text = st.text_input(label = 'Text')
+  handbook_text_language = st.text_input(label = 'Language', value = 'en')
+  handbook_hits = st.text_input(label = 'Hits', value = 0)
     
-    uploaded_file = st.file_uploader(label = "Upload a picture", type = 'png')
+  uploaded_file = st.file_uploader(label = "Upload a picture", type = 'png')
         
-    if uploaded_file is not None:
-      handbook_image = uploaded_file.getvalue()
+  if uploaded_file is not None:
+    handbook_image = uploaded_file.getvalue()
               
-    else:
-      handbook_image = loadFile("images/placeholder_documentation.png")
+  else:
+    handbook_image = loadFile("images/placeholder_documentation.png")
       
     
-    ## Submit button
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-      # Write entry to table `HANDBOOK_USER`
-      query = "INSERT INTO `benbox`.`HANDBOOK_USER`(ID, HANDBOOK_ID, CATEGORY_ID, CATEGORY_SUB_ID, HANDBOOK_CHAPTER, HANDBOOK_PARAGRAPH, HANDBOOK_KEYWORD1, HANDBOOK_KEYWORD2, HANDBOOK_KEYWORD3, HANDBOOK_KEYWORD4, HANDBOOK_KEYWORD5, HANDBOOK_SUMMARY, HANDBOOK_TEXT, HANDBOOK_TEXT_LANGUAGE, HANDBOOK_HITS) VALUES (%s, '%s', '%s', '%s', %s, %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" %(id, handbook_id, categories_id[category], sub_categories_id[sub_category], handbook_chapter, handbook_paragraph, handbook_keyword1, handbook_keyword2, handbook_keyword3, handbook_keyword4, handbook_keyword5, handbook_summary, handbook_text, handbook_text_language, handbook_hits)
-      st.write(query)
-      run_query(query)
-      conn.commit()
+  ## Submit button
+  submitted = st.form_submit_button("Submit")
+  if submitted:
+    # Write entry to table `HANDBOOK_USER`
+    query = "INSERT INTO `benbox`.`HANDBOOK_USER`(ID, HANDBOOK_ID, CATEGORY_ID, CATEGORY_SUB_ID, HANDBOOK_CHAPTER, HANDBOOK_PARAGRAPH, HANDBOOK_KEYWORD1, HANDBOOK_KEYWORD2, HANDBOOK_KEYWORD3, HANDBOOK_KEYWORD4, HANDBOOK_KEYWORD5, HANDBOOK_SUMMARY, HANDBOOK_TEXT, HANDBOOK_TEXT_LANGUAGE, HANDBOOK_HITS) VALUES (%s, '%s', '%s', '%s', %s, %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s);" %(id, handbook_id, categories_id[category], sub_categories_id[sub_category], handbook_chapter, handbook_paragraph, handbook_keyword1, handbook_keyword2, handbook_keyword3, handbook_keyword4, handbook_keyword5, handbook_summary, handbook_text, handbook_text_language, handbook_hits)
+    run_query(query)
+    conn.commit()
       
-      # Upload picture to database
-      pictureUploader(handbook_image, id)
-
-  
-## Column 1
-with col2:
-  st.image("images/Ben.png")
+    # Upload picture to database
+    pictureUploader(image = handbook_image, index = id)
+    
+    # Rerun
+    st.experimental_rerun()
