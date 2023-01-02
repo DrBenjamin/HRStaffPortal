4ce77215bbdea15f5306cab7d6fbafed9c56baba
Benjamin Gross <benjamin.gross@giz.de>
2023-01-02 10:02:55 +0100

    removed Prototype tag

--- 🏥_HR_Staff_Portal.py
+++ 🏥_HR_Staff_Portal.py
@@ -1,5 +1,5 @@
 ##### `🏥_HR_Staff_Portal.py`
-##### HR Staff Portal Prototype
+##### HR Staff Portal
 ##### Open-Source, hostet on https://github.com/DrBenjamin/HRStaffPortal
 ##### Please reach out to benjamin.gross@giz.de for any questions
 #### Loading needed Python libraries
