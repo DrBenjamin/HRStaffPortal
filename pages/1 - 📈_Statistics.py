2a1c0b16edd0122e5b58ed4be655caf8fb674b97
Benjamin Gross <benjamin.gross@giz.de>
2023-01-13 13:00:13 +0100

    added statistics (job, gender, nationality, marriage, age)

--- pages/1 - 📈_Statistics.py
+++ pages/1 - 📈_Statistics.py
@@ -8,13 +8,15 @@ import streamlit.components.v1 as stc
 import pandas as pd
 import numpy as np
 import sys
+import mysql.connector
+from datetime import date
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 sys.path.insert(1, "pages/functions/")
 from functions import header
 from functions import check_password
 from functions import logout
 from functions import landing_page
-sys.path.insert(2, "files/")
-from Code_Snippets import diffuser
 
 
 
@@ -47,6 +49,33 @@ if ('header' not in st.session_state):
 
 
 
+#### Functions
+### Function: run_query = Initial SQL Connection
+def init_connection():
+  ## Initialize connection
+  try:
+    return mysql.connector.connect(**st.secrets["mysql"])
+  except:
+    print("An exception occurred in function `init_connection`")
+    st.error(body = 'Databank connection timeout!', icon = "🚨")
+    st.stop()
+
+
+
+### Function: run_query = SQL query
+def run_query(query):
+  with conn.cursor() as cur:
+    # Perform query
+    try:
+      cur.execute(query)
+      return cur.fetchall()
+    
+    except:
+      print('An exception occurred in function `run_query` with query \"' + query + '\"')
+  
+
+
+
 #### Main program
 ### Logged in state (Statistics)
 if check_password():
@@ -56,114 +85,174 @@ if check_password():
   
   
   ### Statistics expander  
-  with st.expander(label = 'Statistics', expanded = True):  
-    ## Download links
-    # Data comes from Helmholtz-Zentrum Potsdam
-    # https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_nowcast.txt
-    # https://www-app3.gfz-potsdam.de/kp_index/Kp_ap_since_1932.txt
-    DATA_URL = 'files/Employees.txt'
+  with st.expander(label = 'Statistics', expanded = True):
+    ## Open databank connection
+    conn = init_connection()
+
+
+    ## Getting employee data
+    query = "SELECT ima.ID, emp.EMPLOYEE_GENDER, emp.EMPLOYEE_BIRTHDAY, ima.JOB_TITLE, emp.EMPLOYEE_NATIONALITY, emp.EMPLOYEE_MARRIAGE_STATUS FROM `idcard`.`IMAGEBASE` As ima LEFT JOIN `idcard`.`EMPLOYEE` AS emp ON emp.EMPLOYEE_NO = ima.EMPLOYEE_NO;"
+    rows = run_query(query)
+    databank_employee = pd.DataFrame(columns = ['ID', 'GENDER', 'BIRTHDAY', 'JOB', 'NATIONALITY', 'MARRIAGE'])
+    for row in rows:
+      df = pd.DataFrame([[row[0], row[1], row[2], row[3], row[4], row[5]]], columns = ['ID', 'GENDER', 'BIRTHDAY', 'JOB', 'NATIONALITY', 'MARRIAGE'])
+      databank_employee = pd.concat([databank_employee, df])
+    databank_employee = databank_employee.set_index('ID')
     
     
-    ## Load data function
-    # Function is cached
-    @st.cache
-    def load_data():
-      colnames = ['Year', 'Month', 'Day', 'Hour', 'Hour_m', 'Days', 'Days_m', 'Kp', 'Employees', 'D']
-      data = pd.read_table(DATA_URL, sep = " ", header = None, names = colnames, skiprows = 31, skipinitialspace = True)
-      return data
+    ## Gender data
+    st.subheader('Gender statistics')
+    gender = []
+    counter = []
+    for sex in databank_employee['GENDER']:
+      not_new = False
+      i = 0
+      for gen in gender:
+        if sex == gen:
+          not_new = True
+          counter[i] += 1
+        i += 1
+      if not_new == False:
+        if sex == None:
+          gender.append('Unknown')
+          counter.append(1)
+        else:
+          gender.append(sex)
+          counter.append(1)
+    gender_data = pd.DataFrame(columns = ['Gender', 'Number'])
+    for i in range(len(gender)):
+      df = pd.DataFrame([[gender[i], counter[i]]], columns = ['Gender', 'Number'])
+      gender_data = pd.concat([gender_data, df])
+    gender_data = gender_data.set_index('Gender')
+    st.bar_chart(gender_data, x = gender)
     
-    # Show loading message
-    #data_load_state = st.text('Loading data...')
-    data = load_data()
-    #data_load_state.text(str('Download completed') + ' (' + str(round(sys.getsizeof(data)/1048576, 2)) + 'MB)!')
-    
-    
-    ## Checkbox for option to see raw data
-    if st.checkbox(str('Show raw data?')):
-      st.subheader('Raw data')
-      st.write(data)
-    
-     
-    ## Create data frames
-    data_plot_today = pd.DataFrame({'Time': data.Hour.astype(int), 'Employees': data.Employees}).tail(8)
-    data_cal = pd.DataFrame({'Date': pd.to_datetime(data.Year.map(str) + "-" + data.Month.map(str) + "-" + data.Day.map(str)), 'Employees': data.Employees})
-    
-    
-    ## Calculation of avg ap per day and top 10 max values
-    # Function is cached
-    @st.cache(allow_output_mutation = True)
-    def calc_top10(date, ap):
-      avg_ap_d = []
-      avg_ap_d_t = []
-      max_ap = [ [0, ''], [0, ''], [0, ''], [0, ''], [0, ''], [0, ''], [0, ''], [0, ''], [0, ''], [0, ''] ]
-      test_str = ""
-      x = 0
-      for i in range(1, len(ap)): 
-        if date[i] == test_str or i == 1:
-          x = x + ap[i]
-          test_str = date[i]
-        else :
-          x = x / 8
-          avg_ap_d_t.append(str(test_str))
-          avg_ap_d.append(int(np.ceil(x)))
-          for y in range(0, 10, 1) :
-            # Creating top 10 list
-            if x > max_ap[y][0] :
-              max_ap[y][0] = int(np.ceil(x))
-              max_ap[y][1] = str(test_str)[0:10]
-              list.sort(max_ap, reverse = False)
-              break
-          x = ap[i]
-          test_str = date[i]
-      return avg_ap_d, avg_ap_d_t, max_ap
-    
-    
-    ## Call function to do the calculation
-    avg_ap_d, avg_ap_d_t, max_ap = calc_top10(date = data_cal['Date'], ap = data_cal['Employees'])
-    # Create dataframe with avg ap per day values
-    data_plot = pd.DataFrame({'Date': pd.to_datetime(avg_ap_d_t),
-                              'Employees': avg_ap_d})
-    
-    
-    ## Plotting
-    # Set indexes (x-axis) to 'Time' and 'Date' column
-    data_plot_today = data_plot_today.set_index('Time')
-    data_plot = data_plot.set_index('Date')
-    # Daily activity
-    st. subheader('Diagram of today`s employee activity')
-    st.bar_chart(data_plot_today)
-    # All data plot
-    st.subheader('Diagram of employee activity last 30 days')
-    st.line_chart(data_plot)
-    
-    
-    ## Show 10 lowest employee days
-    # Reverse 'False' -> order with lowest first
-    list.sort(max_ap, reverse = False)
-    # Create data frame
-    max_ap_data = pd.DataFrame()
-    max_ap_data['Date'] = [sublist[1] for sublist in max_ap]
-    max_ap_data['Employees'] = [sublist[0] for sublist in max_ap]
-    max_ap_data_index = pd.Index(range(1, 11, 1))
-    max_ap_data = max_ap_data.set_index(max_ap_data_index)
-    # Show Lowest 10
-    st.write('The minimum of the daily employee activity was on ', str(max_ap[0][1]), ' at ', str(max_ap[0][0]), 'employees')
-    if st.checkbox('Show minimum days?'):
-      st.subheader('Days with the lowest number of staff')
-      st.write
-      
   
-  
-  ### Image diffusion
-  ## Text input
-  prompt = st.text_input('What kind of photo should the AI produce?')
-  
-  
-  ## Do the diffusion
-  if prompt != '':
-    if st.session_state['prompt'] != prompt:
-      diffuser(prompt)
-      st.session_state['prompt'] = prompt
+    ## Job data
+    st.subheader('Job statistics')
+    jobs = []
+    counter = []
+    for job in databank_employee['JOB']:
+      not_new = False
+      i = 0
+      for j in jobs:
+        if j == job:
+          not_new = True
+          counter[i] += 1
+        i += 1
+      if not_new == False:
+        if job == None:
+          jobs.append('Unknown')
+          counter.append(1)
+        else:
+          jobs.append(job)
+          counter.append(1)
+    job_data = pd.DataFrame(columns = ['Job', 'Number'])
+    for i in range(len(jobs)):
+      df = pd.DataFrame([[jobs[i], counter[i]]], columns = ['Job', 'Number'])
+      job_data = pd.concat([job_data, df])
+    job_data = job_data.set_index('Job')
+    st.bar_chart(job_data, x = jobs)
+    
+    
+    ## Nationality data
+    st.subheader('Nationality statistics')
+    nationality = []
+    counter = []
+    for nation in databank_employee['NATIONALITY']:
+      not_new = False
+      i = 0
+      for n in nationality:
+        if n == nation:
+          not_new = True
+          counter[i] += 1
+        i += 1
+      if not_new == False:
+        if nation == None:
+          nationality.append('Unknown')
+          counter.append(1)
+        else:
+          nationality.append(nation)
+          counter.append(1)
+    nationality_data = pd.DataFrame(columns = ['Nationality', 'Number'])
+    for i in range(len(nationality)):
+      df = pd.DataFrame([[nationality[i], counter[i]]], columns = ['Nationality', 'Number'])
+      nationality_data = pd.concat([nationality_data, df])
+    nationality_data = nationality_data.set_index('Nationality')
+    st.bar_chart(nationality_data, x = nationality)
+
+
+    ## Marriage data
+    st.subheader('Marriage data')
+    marriage = []
+    counter = []
+    for married in databank_employee['MARRIAGE']:
+      not_new = False
+      i = 0
+      for n in marriage:
+        if n == married:
+          not_new = True
+          counter[i] += 1
+        i += 1
+      if not_new == False:
+        if married == None:
+          marriage.append('Unknown')
+          counter.append(1)
+        else:
+          marriage.append(married)
+          counter.append(1)
+    marriage_data = pd.DataFrame(columns = ['Marriage', 'Number'])
+    for i in range(len(marriage)):
+      df = pd.DataFrame([[marriage[i], counter[i]]], columns = ['Marriage', 'Number'])
+      marriage_data = pd.concat([marriage_data, df])
+    marriage_data = marriage_data.set_index('Marriage')
+    st.bar_chart(marriage_data, x = marriage)
+ 
+    
+    ## Age data
+    st.subheader('Age data')
+    age = []
+    counter = []
+    twenty_yrs_ago = date.today() - relativedelta(years = 20)
+    thirty_yrs_ago = date.today() - relativedelta(years = 30)
+    fourty_yrs_ago = date.today() - relativedelta(years = 40)
+    fifty_yrs_ago = date.today() - relativedelta(years = 50)
+    sisty_yrs_ago = date.today() - relativedelta(years = 60)
+    for old in databank_employee['BIRTHDAY']:
+      if old != None:
+        if old < sisty_yrs_ago:
+          old_cat = '+60y'
+        elif old < fifty_yrs_ago:
+          old_cat = '50-60y'
+        elif old < fourty_yrs_ago:
+          old_cat = '40-50y'
+        elif old < thirty_yrs_ago:
+          old_cat = '30-40y'
+        elif old < twenty_yrs_ago:
+          old_cat = '20-30y'
+        else:
+          old_cat = '<20y'
+      else:
+        old_cat = 'Unknown'
+      not_new = False
+      i = 0
+      for a in age:
+        if a == old_cat:
+          not_new = True
+          counter[i] += 1
+        i += 1
+      if not_new == False:
+        if old == None:
+          age.append('Unknown')
+          counter.append(1)
+        else:
+          age.append(old_cat)
+          counter.append(1)
+    age_data = pd.DataFrame(columns = ['Age', 'Number'])
+    for i in range(len(age)):
+      df = pd.DataFrame([[age[i], counter[i]]], columns = ['Age', 'Number'])
+      age_data = pd.concat([age_data, df])
+    age_data = age_data.set_index('Age')
+    st.bar_chart(age_data, x = age)
 
 
     
