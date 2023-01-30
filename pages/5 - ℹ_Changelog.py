d8da78be6e2f22054c7770decb229bda7289ff8f
Benjamin Gross <ben@benbox.org>
2023-01-30 13:02:53 +0200

    added `CHANGELOG.md` integration

--- pages/5 - ℹ_Changelog.py
+++ pages/5 - ℹ_Changelog.py
@@ -73,7 +73,7 @@ with st.expander('Changelog', expanded = True):
   #source = "http://192.168.1.173/index.html"
   #st.write(source)
   stc.iframe(src = "http://192.168.1.173/index.html", height = 520, scrolling = True)
-  stc.html("""<iframe src="http://127.0.0.1/index.html" style = "max-width: 100%; border: 0" width = "600"></iframe>""", height = 520) 
+  stc.html("""<iframe src="http://192.168.1.173/index.html" style = "max-width: 100%; border: 0" width = "600"></iframe>""", height = 520) 
     
     
     
