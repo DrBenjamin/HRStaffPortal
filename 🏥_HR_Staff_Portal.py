fcaf6b0a0e52005cbe035a0aeed4064064405998
Benjamin Gross <benjamin.gross@giz.de>
2023-02-03 10:25:06 +0200

    removed debugging

--- 🏥_HR_Staff_Portal.py
+++ 🏥_HR_Staff_Portal.py
@@ -29,7 +29,6 @@ from functions import landing_page
 #### Streamlit initial setup
 desc_file = open('DESCRIPTION', 'r')
 lines = desc_file.readlines()
-print(lines[3])
 st.set_page_config(
   page_title = "HR Staff Portal",
   page_icon = st.secrets['custom']['facility_image_thumbnail'],
