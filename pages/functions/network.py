b136f4bef30674a89e12548ca13ad3d76516f5b8
DrBenjamin <ben@benbox.org>
2023-03-17 09:43:26 +0200

    removed `loguru` module

--- pages/functions/network.py
+++ pages/functions/network.py
@@ -1,6 +1,6 @@
 ##### `/pages/functions/network.py`
 ##### HR Staff Portal
-##### Open-Source, hostet on https://github.com/DrBenjamin/HRStaffPortal
+##### Open-Source, hosted on https://github.com/DrBenjamin/HRStaffPortal
 ##### Please reach out to benjamin.gross@giz.de for any questions
 #### Loading needed Python libraries
 import streamlit as st
