b8c7391b5081615adf02ed52fc984190856a66b2
Benjamin Gross <ben@benbox.org>
2023-04-18 11:54:26 +0200

    updated to new database structure

--- files/idcard_dump.sql
+++ files/idcard_dump.sql
@@ -4,6 +4,8 @@
 -- ------------------------------------------------------
 -- Server version	10.5.18-MariaDB-0+deb11u1
 
+USE idcard;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
